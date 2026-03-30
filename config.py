8d8430b29c265a36c0963a1148fb30e1e20ffe6d
Sourav Agarwal <souravagarwal694g@gmail.com>
2026-03-30 18:23:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,8 +27,8 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "40"))
 #--- ---- ---- --- --- --- - -- -  - - - - - - - - - - - --  - -
 
 # Start pic
-START_PIC = "https://telegra.ph/file/f3d3aff9ec422158feb05-d2180e3665e0ac4d32.jpg"
-START_IMG = "https://telegra.ph/file/f3d3aff9ec422158feb05-d2180e3665e0ac4d32.jpg"
+START_PIC = "https://i.ibb.co/7NKb0547/x.jpg"
+START_IMG = "https://i.ibb.co/7NKb0547/x.jpg"
 # Messages
 START_MSG = os.environ.get("START_MESSAGE", "<b>ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴛʜᴇ ᴀᴅᴠᴀɴᴄᴇᴅ ʟɪɴᴋs sʜᴀʀɪɴɢ ʙᴏᴛ. ᴡɪᴛʜ ᴛʜɪs ʙᴏᴛ, ʏᴏᴜ ᴄᴀɴ sʜᴀʀᴇ ʟɪɴᴋs ᴀɴᴅ ᴋᴇᴇᴘ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟs sᴀғᴇ ғʀᴏᴍ ᴄᴏᴘʏʀɪɢʜᴛ ɪssᴜᴇs.\n\n<blockquote>‣ ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://t.me/Im_sukuna02'>Sᴜᴋᴜɴᴀ</a></blockquote></b>")
 HELP = os.environ.get("HELP_MESSAGE", "<b><blockquote expandable>» Creator: <a href=https://t.me/Im_Sukuna02>Sukuna</a>\n» Our Community: <a href=https://t.me/InfinixSyndicate>ɪɴꜰɪɴɪx ꜱʏɴᴅɪᴄᴀᴛᴇ</a>\n» Anime Channel: <a href=https://t.me/AnimeInfinix>Anime Infinix</a>\n» Ongoing Anime: <a href=https://t.me/Ongoing_Infinix>Ongoing Infinix</a>\n» Developer: <a href=https://t.me/im_sukuna02>Sᴜᴋᴜɴᴀ</a></b>")
